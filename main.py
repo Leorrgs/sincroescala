f79a3e7d1086ffd31080fd63601b190f86c11817
Leorrgs <henrique1915rodrigue@gmail.com>
2026-09-11 19:50:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from funcionario import cadastrar_funcionários, listar_funcionários, excluir_funcionários
-from escala import cadastrar_escala
+from escala import cadastrar_escala, excluir_escala, listar_escalas
 
 sair_do_sistema = False
 
@@ -11,10 +11,13 @@ def apresenta_menu():
     print("1. Cadastrar funcionário📝")
     print("2. Listar funcionário📝📝")
     print("3. Excluir funcinário🗑️")
-    print("4. Criar escala📈")
+    print("4. Criar escalas📈")
+    print("5. Listar escalas📊")
+    print("6. Excluir escalas")
     print("0. Sair ❌")
     print("")
     opcao_menu = input("Escolha uma opção: ")
+    print("")
     return opcao_menu
      
 def sair():
@@ -30,8 +33,12 @@ while not sair_do_sistema:
             listar_funcionários()
         case "3":
             excluir_funcionários()  
-        case "4:":
-            cadastrar_escala()      
+        case "4":
+            cadastrar_escala()  
+        case "5":
+            listar_escalas()     
+        case "6":
+            excluir_escala()      
         case "0":
             sair()
             break

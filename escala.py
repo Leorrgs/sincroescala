c911a63f1eeaf43b2af868f4e2b7b5c11bbd7541
Leorrgs <henrique1915rodrigue@gmail.com>
2026-09-18 19:47:51 -0300

    Fix path and function name for listing scales

--- escala.py
+++ escala.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 
-path_bd = Path("sincroescala/BD") / "escala_bd.txt"
+path_bd = Path("sincro_escala/BD") / "escala_bd.txt"
 escalas = []
 
 def cadastrar_escala():
     escala = input("digite o nome da escala: ")
     with open(path_bd,"a", encoding="utf-8") as arquivo:
             arquivo.write(f"{escala}\n")
-    escalas.append(escala)
     print(f"A escala cadastrada foi: {escala}")
     print("======================================")
     print("você gostaria de cadastrar novas escalas?")
@@ -20,14 +19,14 @@ def cadastrar_escala():
     if seguir_escala== "2":
         print("escala concluida ✅")
         
-def listar_escalas():
-     with open(path_bd,"r", encoding="utf-8") as arquivo:
+def listar_escala():
+    with open(path_bd,"r", encoding="utf-8") as arquivo:
         for linha in arquivo:
             nome_limpo = linha.strip()
-            print(nome_limpo)
-            
+            print(nome_limpo) 
+
 def excluir_escala():
-    listar_escalas()
+    listar_escala()
     escala = input("qual escala você deseja deletar: ")
     with open(path_bd,"r", encoding="utf-8") as arquivo:
         nomes = arquivo.readlines()
@@ -35,5 +34,5 @@ def excluir_escala():
         for linha in nomes :
             if linha.strip() == escala:
                 linha = ""
-            arquivo.write(linha)              
-                
+            arquivo.write(linha)                     
+            
